c92d184563c919e2661fa2fb00755894f6cf4c69
pro17-grammer <pro.grammer1799@gmail.com>
2024-11-26 11:44:36 +0000

    basics Js examples

---
+++ basics/basics.js
@@ -0,0 +1,17 @@
+let name = "Spidername";
+name = "Ironman"
+
+// console.log(name);
+
+
+// console.log(typeof undefined);
+// console.log(typeof null);
+
+let arr = [1,2,3,4,5];
+
+console.log(arr.slice(0,4));
+console.log(arr);
+
+
+console.log((arr.splice(0,3)));
+console.log(arr);
